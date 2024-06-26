c4e231b27fbe530f6873d57f88200b390be9e839
Jack Weilage <jack@weilage.dev>
2024-06-25 19:30:39 -0700

    Fix flatpaks not being in `$XDG_DATA_DIRS`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,6 @@ set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -gx XDG_STATE_HOME "$HOME/.local/state"
-set -gx XDG_DATA_DIRS /usr/share
 set -gx XDG_CONFIG_DIRS /etc/xdg
 set -gx XDG_RUNTIME_DIR "/run/user/$(id -u)"
 
