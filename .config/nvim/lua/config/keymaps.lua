dea720789e767125bd86b0c103fdae336555789c
Jack Weilage <jack@weilage.dev>
2024-10-04 21:28:14 -0700

    Actually include proper keybinds for `smart-splits`

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local splits = require("smart-splits")
+vim.keymap.set("n", "<A-h>", splits.resize_left)
+vim.keymap.set("n", "<A-j>", splits.resize_down)
+vim.keymap.set("n", "<A-k>", splits.resize_up)
+vim.keymap.set("n", "<A-l>", splits.resize_right)
+
+vim.keymap.set("n", "<C-h>", splits.move_cursor_left)
+vim.keymap.set("n", "<C-j>", splits.move_cursor_down)
+vim.keymap.set("n", "<C-k>", splits.move_cursor_up)
+vim.keymap.set("n", "<C-l>", splits.move_cursor_right)
